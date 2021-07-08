34fab9e6c69655059b172331f9755c85dd9a9c01
Marshmillo <johancamilodiaz@gmail.com>
2021-07-07 23:27:37 -0500

    feat(subnet): create subnet

--- resources.tf
+++ resources.tf
@@ -7,6 +7,17 @@ resource "aws_vpc" "milo_vpc" {
     }
 }
 
+resource "aws_subnet" "subnet_milo" {
+    vpc_id = aws_vpc.milo_vpc.id
+    cidr_block = "10.0.0.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "Milo Subnet Terraform"
+    }
+}
+
 #resource "aws_instance" "milo_instance" {
 #  ami = "ami-0ab4d1e9cf9a1215a"
 #  instance_type = "t2.micro"
