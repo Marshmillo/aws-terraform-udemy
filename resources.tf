7268ea176376a11715531284b2aee1923db35011
Marshmillo <johancamilodiaz@gmail.com>
2021-07-07 23:06:42 -0500

    feat(gitignore): add gitignore

--- resources.tf
+++ resources.tf
@@ -1,4 +1,13 @@
-resource "aws_instance" "milo_instance" {
-  ami = "ami-0ab4d1e9cf9a1215a"
-  instance_type = "t2.micro"
+resource "aws_vpc" "milo_vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    
+    tags = {
+        Name = "Milo VPC Terraform"
+    }
 }
+
+#resource "aws_instance" "milo_instance" {
+#  ami = "ami-0ab4d1e9cf9a1215a"
+#  instance_type = "t2.micro"
+#}
